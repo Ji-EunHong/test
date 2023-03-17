4dbf38242fb3f33ac0b1dad1fe7ed3aec991ce09
Ji-EunHong <119845110+Ji-EunHong@users.noreply.github.com>
2023-03-18 08:14:12 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.scss";
+import $ from "jquery";
+
+async function asyncCB(event) {
+  event.preventDefault();
+  let url =
+    "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits";
+  let response = await fetch(url);
+
+  let commits = await response.json(); // read response body and parse as JSON
+
+  console.log(commits[0].author.login);
+}
+
+$("body").on("reinitialize", function () {
+  $("a").off("click", asyncCB);
+  $("a").on("click", asyncCB);
+});
+
+setTimeout(() => {
+  $("body").trigger("reinitialize");
+});
+
+setTimeout(() => {
+  $("body").trigger("reinitialize");
+}, 2000);
